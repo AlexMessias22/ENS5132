00314a8682930ab0122f95e1c16bcf96df4d395d
AlexMessias22 <messiasjuoficial@gmail.com>
2025-04-06 14:12:19 -0300

    oi

---
+++ scripts/aula03.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Este script foi criado na terceira aula da disciplina ENS5132 Nesta aula 
+trabalhamos:
+    - Arroys numpy
+    - Pandas 
+    - Matplotlib
+    
+    *requisitos: pip install spyder numpy pandas matplotlib
+    
+This is a temporary script file.
+"""
+#%% Importação de Pacotes
+import numpy as np
+import pandas as pd
+import matplotlib .pyplot as plt
+
+#%% relembrando listas
+
+listA = [1,2,3 , 'Salve o Corinthias' , 20,5]
+print(listA)
+
+#%% Trabalhando com numpy
+
+# Criando  um array numpy
+
+array = np.array[(0.7, 0.75, 1.85)]
+
+
+
+
+         
+    
+
+
